98ca7702ed56f091f4926edcc687e82e8ce591cb
Muhammad Zulqarnain Abdullah <muhammadzulqarnain1559@gmail.com>
2025-03-28 11:45:00 +0500

    Add Swagger and OpenAPI support

--- studentApi/Program.cs
+++ studentApi/Program.cs
@@ -1,6 +1,20 @@
+// Program.cs
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
